a64d5203c752b14d6d9029f5c8e705dcd0a23330
Nishit <nishitvataliya003@gmail.com>
2025-03-13 13:41:28 +0530

    Initial commit - TaskTrackPro project

---
+++ MyApp/MyApp.Core/Repositories/Implementations/UserRepository.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MyApp.Core.Repositories.Interfaces;
+
+namespace MyApp.Core.Repositories.Implementations
+{
+    public class UserRepository : IUserInterface
+    {
+        
+    }
+}
