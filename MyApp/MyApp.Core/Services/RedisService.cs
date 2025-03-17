b81a2b2ff5798b2cb54cc9cd4c2b3731299c4cf1
harsh lakhani <harshlakhanip@gmail.com>
2025-03-17 22:16:10 +0530

    added api for list task view with redis

--- MyApp/MyApp.Core/Services/RedisService.cs
+++ MyApp/MyApp.Core/Services/RedisService.cs
@@ -1,12 +1,41 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
+using MyApp.Core.Model;
+using MyApp.Core.Repositories.Interfaces;
+using StackExchange.Redis;
 
 namespace MyApp.Core.Services
 {
     public class RedisService
     {
-        
+        private readonly IConnectionMultiplexer _redis;
+
+        private readonly IUserInterface _userServices;
+        public RedisService(IConnectionMultiplexer redis, IUserInterface userInterface)
+        {
+            _redis = redis;
+            _userServices = userInterface;
+        }
+
+        public void SetTaskList(int userId, List<t_task_user> taskData)
+        {
+            var db = _redis.GetDatabase();
+            string json = JsonSerializer.Serialize(taskData);
+            db.StringSet($"TaskList:{userId}", json);
+        }
+
+        public async Task<List<t_task_user>> GetTaskList(int userId)
+        {
+            var db = _redis.GetDatabase();
+            string json = await db.StringGetAsync($"TaskList:{userId}");
+            if (json == null)
+            {
+                return new List<t_task_user>();
+            }
+            return JsonSerializer.Deserialize<List<t_task_user>>(json);
+        }
     }
 }
